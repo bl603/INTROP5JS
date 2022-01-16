6ea086f2902efce7469111d421e3beafc0c8e276
bl603 <80650591+bl603@users.noreply.github.com>
2022-01-16 13:42:33 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function reload(){
+
+}
+function setup(){
+    canvas=createCanvas(640,478);
+    canvas.position(110,244);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_color="";
+}
+
+function draw(){
+    image(video,0,0,640,481);
+    tint(tint_color);
+}
+function take_snapshot(){
+    save('student_name.png');
+}
+
+function filter_tint(){
+    tint_color=document.getElementById("color_name").value;
+}
